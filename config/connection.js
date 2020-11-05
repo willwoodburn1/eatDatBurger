78594e9aa9cda0e05423ec05ae1adbadc365caf3
User <user@Users-MacBook-Pro.local>
2020-11-05 19:38:59 +1000

    changed env file

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ var mysql = require("mysql");
 
 
 var connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 
